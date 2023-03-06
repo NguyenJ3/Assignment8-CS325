d2c5aec975af46806d765037809e634e0302eeef
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-03-06 14:32:58 -0800

    Logic

--- MST.py
+++ MST.py
@@ -4,3 +4,6 @@ def Prims(G):
     key = [float('inf')] * n
     parent = [None] * n
     key[0] = 0
+    for i in range(n):
+        u = min((key[j], j) for j in range(n) if not visited[j])[1]   # select the vertex with minimum key value that is not visited
+        visited[u] = True           # mark the selected vertex as visited
