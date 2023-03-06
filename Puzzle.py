6ce2e1a80ae7309bff2294aac6642f14d90942a5
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-03-06 15:38:25 -0800

    Logic

--- Puzzle.py
+++ Puzzle.py
@@ -8,3 +8,10 @@ def solve_puzzle(Board, Source, Destination):
         # check if the current cell is the destination, and return the path if it is
         if (i, j) == Destination:
             return path
+        for ni, nj in ((i-1, j), (i, j+1), (i+1, j), (i, j-1)):
+            if 0 <= ni < rows and 0 <= nj < cols and Board[ni][nj] == '-' and (ni, nj) not in visited:
+                # mark cell as visited and add it to the queue with its path
+                visited.add((ni, nj))
+                queue.append(((ni, nj), path + [(ni, nj)]))
+    # if no path to destination is found, return None
+    return None
