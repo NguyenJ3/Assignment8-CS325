2a19d02779777036c45173ac07b0ba9f1a319a6f
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-03-06 15:24:03 -0800

    While loop

--- Puzzle.py
+++ Puzzle.py
@@ -2,3 +2,9 @@ def solve_puzzle(Board, Source, Destination):
     rows, cols = len(Board), len(Board[0])
     queue = [(Source, [Source])]
     visited = set()
+    while queue:
+        # get the current cell and its path from the queue
+        (i, j), path = queue.pop(0)
+        # check if the current cell is the destination, and return the path if it is
+        if (i, j) == Destination:
+            return path
